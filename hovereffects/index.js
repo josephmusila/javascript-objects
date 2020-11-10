1e83e6fe903760ade8d5e30a3a152a4648c249ea
joseph musila <josemusila03@gmail.com>
2020-11-10 21:24:40 +0300

    commit

---
+++ hovereffects/index.js
@@ -0,0 +1,25 @@
+let names = [
+	{
+		name: "jose",
+		age: 20,
+		
+
+		skill: "dev",
+	},
+	{
+		name: "jmusila",
+		age: 25,
+		skill: "tester",
+	},
+	{
+		name: "jela",
+		age: 30,
+		skill: "pmanager",
+	},
+	{
+		name: "munguti",
+		age: 40,
+		skill: "designer",
+	},
+];
+
